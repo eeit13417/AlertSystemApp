7888022f83227a240ada0f6dcf1e43dfcda039f6
eeit13417 <minwen0734@gmail.com>
2025-08-13 17:37:30 +1000

    AL-6 < Backend API - Implement update product functionality. >

--- backend/controllers/stockController.js
+++ backend/controllers/stockController.js
@@ -30,24 +30,24 @@ const addStock = async (req, res) => {
 };
 
 
-// const updateStock = async (req, res) => {
-//     const {  title, quantity, createDate, status, updateDate } = req.body;
-//     console.log(req.body)
-//     const managerId = req.admin.id
-//     try {
-//         const stock = await Stock.findById(req.params.id);
-//         if (!stock) return res.status(404).json({ message: 'Task not found' });
-//         stock.title = title || stock.title;
-//         stock.quantity = quantity || stock.quantity; 
-//         stock.createDate = createDate || stock.createDate; 
-//         stock.status = status; 
-//         stock.updateDate = updateDate || stock.updateDate;
-//         stock.managerId = managerId || stock.managerId;
-//         const updatedStock = await stock.save();
-//         res.json(updatedStock);
-//     } catch (error) {
-//         res.status(500).json({ message: error.message });
-//     }
-// };
+const updateStock = async (req, res) => {
+    const {  title, quantity, createDate, status, updateDate } = req.body;
+    console.log(req.body)
+    const managerId = req.admin.id
+    try {
+        const stock = await Stock.findById(req.params.id);
+        if (!stock) return res.status(404).json({ message: 'Task not found' });
+        stock.title = title || stock.title;
+        stock.quantity = quantity || stock.quantity; 
+        stock.createDate = createDate || stock.createDate; 
+        stock.status = status; 
+        stock.updateDate = updateDate || stock.updateDate;
+        stock.managerId = managerId || stock.managerId;
+        const updatedStock = await stock.save();
+        res.json(updatedStock);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
 
 module.exports = { getStocks, checkLowStock, addStock, updateStock };
