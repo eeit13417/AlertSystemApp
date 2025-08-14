2708094798aeb97cfee1375c3e536a75bf32d18f
eeit13417 <minwen0734@gmail.com>
2025-08-14 12:03:13 +1000

    AL-21 < Backend API - Implement delete admin functionality.>

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const Admin = require('../models/admin');
 
 const protect = async (req, res, next) => {
     let token;
@@ -9,7 +9,7 @@ const protect = async (req, res, next) => {
         try {
             token = req.headers.authorization.split(' ')[1];
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            req.user = await User.findById(decoded.id).select('-password');
+            req.admin = await Admin.findById(decoded.id).select('-password');
             next();
         } catch (error) {
             res.status(401).json({ message: 'Not authorized, token failed' });
