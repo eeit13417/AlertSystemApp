8884a64328b5784432754ec14f4767ff7d0504ed
eeit13417 <minwen0734@gmail.com>
2025-08-16 21:54:57 +1000

    changed in testing and workflows

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://52.65.237.110:5001', // live
+  baseURL: 'http://3.25.83.49:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
