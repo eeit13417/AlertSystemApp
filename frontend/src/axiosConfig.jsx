f53f289521968ed252ec4c8d3989958b7039f5cd
eeit13417 <minwen0734@gmail.com>
2025-08-16 23:50:15 +1000

    AL-16 < Backend API - Determine if inventory is below restock threshold.>

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -4,6 +4,7 @@ const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5001', // local
   baseURL: 'http://3.25.83.49:5001', // live
   headers: { 'Content-Type': 'application/json' },
+  
 });
 
 export default axiosInstance;
